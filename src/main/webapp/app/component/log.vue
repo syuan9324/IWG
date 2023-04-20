ea99a21371274cf1f7efa43e56bf6d9cca982994
eason <2202028@iisigroup.com>
2023-04-20 16:18:45 +0800

    嵌入bootstrap

--- src/main/webapp/app/component/log.vue
+++ src/main/webapp/app/component/log.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
-    <h1>Login Page</h1>
-    <form v-on:submit.prevent="submitForm">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" v-model="username" />
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" v-model="password" />
-      <button type="submit">Login</button>
-      {{ jwt }}
-    </form>
+    <h1 class="text-center">
+      <!-- <font-awesome-icon icon="search"></font-awesome-icon> -->
+      批次服務查詢
+    </h1>
+    <button class="btn btn-danger">删除</button>
+    <b-button variant="info" size="sm">修改</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
+import { ref, computed, reactive, onMounted } from "vue";
+import { BButton } from "bootstrap-vue-3";
 import axios from "axios";
+// import appHeader from "./header/app-header.vue";
 
 export default {
-  name: "home",
+  name: "log",
+  components: { BButton },
   setup() {
     let username = ref("");
     let password = ref("");
+    let batchServiceCategory = ref("");
     let jwt = ref("");
     async function submitForm() {
       if (username.value === "" || password.value === "") {
@@ -37,6 +41,7 @@ export default {
       password,
       submitForm,
       jwt,
+      batchServiceCategory,
     };
   },
 };
