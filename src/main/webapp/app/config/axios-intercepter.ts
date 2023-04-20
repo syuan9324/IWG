2c6c78382b2aa74e3e247d98ecd72307c81314e3
tailinh <tailin.huang@iisigroup.com>
2023-04-20 10:37:50 +0800

    add axios-intercepter for login and error handle

---
+++ src/main/webapp/app/config/axios-intercepter.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const TIMEOUT = 1000000;
+
+//在帳號登入成功後會將token偷偷放在session storage裡面
+//在透過axios interceptor將存起來的token拿出來,放在header裡面
+const onRequestSuccess = (config: any) => {
+  const token =
+    localStorage.getItem("token") || sessionStorage.getItem("token");
+  if (token) {
+    if (!config.headers) {
+      config.headers = {};
+    }
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  config.timeout = TIMEOUT;
+  config.url = `${config.url}`;
+  return config;
+};
+
+const setupAxiosInterceptors = (onUnauthenticated: any, onServerError: any) => {
+  const onResponseError = (err: any) => {
+    const status = err.status || err.response.status;
+    if (status === 403 || status === 401) {
+      return onUnauthenticated(err);
+    }
+    if (status >= 500) {
+      return onServerError(err);
+    }
+    return Promise.reject(err);
+  };
+
+  if (axios.interceptors) {
+    axios.interceptors.request.use(onRequestSuccess);
+    axios.interceptors.response.use((res) => res, onResponseError);
+  }
+};
+
+export { onRequestSuccess, setupAxiosInterceptors };
