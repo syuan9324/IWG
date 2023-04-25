13c74c19817c171a9faaf810f31e1b6ad46a6be2
tailinh <tailin.huang@iisigroup.com>
2023-04-25 10:23:00 +0800

    加入schedule 完成dev模式的基本比對 但路徑都是hard code在程式碼上，之後要改成從DB拿

--- src/main/java/com/iisi/patrol/webGuard/service/ScheduledTaskService.java
+++ src/main/java/com/iisi/patrol/webGuard/service/ScheduledTaskService.java
@@ -1,9 +1,17 @@
 package com.iisi.patrol.webGuard.service;
 
+import com.iisi.patrol.webGuard.WebGuardApplication;
 import com.iisi.patrol.webGuard.service.sshService.ConnectionConfig;
+import com.jcraft.jsch.JSchException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -15,40 +23,63 @@ public class ScheduledTaskService {
 
     private final ConnectionConfigService connectionConfigService;
 
-    private final CommonSSHUtils commonSSHUtils;
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTaskService.class);
 
-    public ScheduledTaskService(ConnectionConfigService connectionConfigService, CommonSSHUtils commonSSHUtils) {
+
+    public ScheduledTaskService(ConnectionConfigService connectionConfigService) {
         this.connectionConfigService = connectionConfigService;
-        this.commonSSHUtils = commonSSHUtils;
     }
 
     /**
      * 測試一下job
      */
-//    @Scheduled(cron = "0/5 * * * * ?")
-    public void testTask(){
+    // @Scheduled(cron = "0/5 * * * * ?")
+    public void testTask() {
         Date date = Calendar.getInstance().getTime();
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         String strDate = dateFormat.format(date);
-        System.out.println(strDate);
+        log.debug(strDate);
     }
 
     /**
-     * 測試一下job
+     * 比對檔案大小
      */
-//    @Scheduled(cron = "0/10 * * * * ?")
-    public void fileComparison(){
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void fileComparisonForDev() {
+        log.info("start file compare");
+        String fileName = "pwc-web.war";
+        String serverLocation = "/home/tailinh/";//在window下啟動， file separator會是'\',會找不到檔案
+        String fromServerLocation = System.getProperty("user.home") + File.separator + "comparison" + File.separator + "fromServer"+ File.separator;
+        String originLocation = System.getProperty("user.home") + File.separator + "comparison" + File.separator + "origin"+ File.separator;
+        log.info("fromServerLocation path :{}",fromServerLocation);
+        log.info("originLocation path :{}",originLocation);
         List<ConnectionConfig> configs = connectionConfigService.getConnectionList();
         configs.stream().forEach(connectionConfig -> {
             try {
-                System.out.println(connectionConfig);
-                String response = CommonSSHUtils.useSshCommand(connectionConfig, "du -B 1 pwc-web.war");
-                System.out.println(response);
+                //String response = CommonSSHUtils.useSshCommand(connectionConfig, "du -B 1 pwc-web.war");
+
+                boolean response = this.fileSizeComparison(connectionConfig, fileName, serverLocation, fromServerLocation, originLocation);
+                if (response) {
+                    long remoteFileSize = new File(fromServerLocation + fileName).length();
+                    long comparedFileSize = new File(originLocation + fileName).length();
+                    log.info("size of origin {} is {},size of {} from server is {}", fileName, comparedFileSize, fileName, remoteFileSize);
+                    log.info("check {} size normal", fileName);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
-
     }
 
+
+    public boolean fileSizeComparison(ConnectionConfig connectionConfig, String fileName, String serverLocation, String localLocation, String originLocation) throws JSchException, IOException {
+        CommonSSHUtils.useScpCopyRemoteFile(connectionConfig, serverLocation, localLocation, fileName);
+        //remote file size
+        long remoteFileSize = new File(localLocation + fileName).length();
+        //comparison file size
+        long comparedFileSize = new File(originLocation + fileName).length();
+
+        return remoteFileSize == comparedFileSize;
+
+    }
 }
---
+++ src/test/java/com/iisi/patrol/webGuard/service/TestSSHService.java
@@ -0,0 +1,117 @@
+package com.iisi.patrol.webGuard.service;
+
+import com.iisi.patrol.webGuard.service.sshService.ConnectionConfig;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class TestSSHService {
+
+
+    /**
+     * JSch Example Tutorial
+     * Java SSH Connection Program
+     */
+    public static void main(String[] args) {
+        String host="192.168.57.202";
+        String user="tailinh";
+        String password="IIsi@940450";
+        String command1="du -B 1 pwc-web.war";
+        ConnectionConfig conn = new ConnectionConfig(host, user, password, 22);
+
+        String response = useSshCommand2(conn, command1);
+        System.out.println(response);
+        String fileName = response.split("\t")[1];
+        String fileSize = response.split("\t")[0];
+        System.out.println(fileName);
+        System.out.println(fileSize);
+    }
+
+
+    public static String useSshCommand(ConnectionConfig connectionConfig, String command){
+        StringBuilder sb = new StringBuilder();
+        try{
+
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+            Session session=jsch.getSession(connectionConfig.getUserName(), connectionConfig.getServerIp(), 22);
+            session.setPassword(connectionConfig.getPassWord());
+            session.setConfig(config);
+            session.connect();
+            System.out.println("Connected");
+
+            Channel channel=session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command);
+            channel.setInputStream(null);
+            ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+            ((ChannelExec)channel).setErrStream(errorStream);
+
+            InputStream in=channel.getInputStream();
+            channel.connect();
+            byte[] tmp=new byte[1024];
+            while(true){
+                while(in.available()>0){
+                    int i=in.read(tmp, 0, 1024);
+                    if(i<0)break;
+                    sb.append(new String(tmp, 0, i));
+                    System.out.print(new String(tmp, 0, i));
+                }
+                if(channel.isClosed()){
+                    System.out.println("exit-status: "+channel.getExitStatus());
+                    break;
+                }
+                try{Thread.sleep(1000);}catch(Exception ee){}
+            }
+            System.out.println(new String(errorStream.toByteArray()));
+            channel.disconnect();
+            session.disconnect();
+            System.out.println("DONE");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public static String useSshCommand2(ConnectionConfig connectionConfig, String command){
+        Session session = null;
+        ChannelExec channel = null;
+        String responseString = null;
+        try {
+            session = new JSch().getSession(connectionConfig.getUserName(), connectionConfig.getServerIp(), connectionConfig.getPort());
+            session.setPassword(connectionConfig.getPassWord());
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand(command);
+            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+            channel.setOutputStream(responseStream);
+            channel.connect();
+
+            while (channel.isConnected()) {
+                Thread.sleep(1000);
+            }
+            responseString = new String(responseStream.toByteArray());
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            if (session != null) {
+                session.disconnect();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+        }
+        return responseString.trim();
+    }
+
+
+    public void checkFileSize(){
+
+    }
+}
